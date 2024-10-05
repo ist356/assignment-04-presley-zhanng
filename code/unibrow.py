8af7c4c7d5400418fe2521f08b41d24a82a1628e
Presley Zhang <presley.s.zhang@gmail.com>
2024-10-05 00:09:48 -0400

    pandaslib completed + little bit of unibrow.py

--- code/unibrow.py
+++ code/unibrow.py
@@ -9,4 +9,9 @@ st.title("UniBrow")
 st.caption("The Universal data browser")
 
 # TODO Write code here to complete the unibrow.py
+''
+uploaded_file = st.file_uploader("Upload dataset.", type = ["csv", "xslx", "json"])
 
+if uploaded_file is not None:
+    ext = pl.get_file_extension(uploaded_file)
+    df = pl.load_file(uploaded_file, ext)
